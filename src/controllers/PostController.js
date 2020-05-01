f06aa968b98e59ae28858d6b997c0a4ef5ac8724
felipealvescosta <felipealves00007@gmail.com>
2020-05-01 16:52:53 -0300

    chore: new files

---
+++ src/controllers/PostController.js
@@ -0,0 +1,27 @@
+const Post = require('../models/Post');
+
+module.exports = {
+  async index(req,res){
+    const posts = await Post.find();
+    return res.json(posts);
+  },
+
+
+  async create(req, res){
+    const {originalname:name, size, key, location: url = '' } = req.file;
+    const post  = await Post.create({
+      name,
+      size,
+      key,
+      url, 
+    });
+    return  res.json(post);
+  },
+
+  
+  async delete(req,res){
+    const post = await Post.findById(req.params.id);
+    await post.remove();
+    return res.send(); 
+  },
+}; 
--- src/routes.js
+++ src/routes.js
@@ -1,29 +1,15 @@
-const routes = require('express').Router();
+const express = require('express');
 const multer  = require("multer"); 
 const multerConfig = require("./config/multer");
+const PostController = require('./controllers/PostController');
 
+const routes  = express.Router(); 
 const Post = require("./models/Post");
 
-routes.get('/post', async(req,res)=>{
-  const posts = await Post.find();
-  return res.json(posts);
-});
+routes.get('/post', PostController.index);
 
-routes.post('/post', multer(multerConfig).single("file") , async (req, res) => {
- const {originalname:name, size, key, location: url = '' } = req.file;
- const post  = await Post.create({
-   name,
-   size,
-   key,
-   url, 
- });
- return  res.json(post);
-});
+routes.post('/post', multer(multerConfig).single("file") , PostController.create);
 
-routes.delete('/posts/:id', async (req,res)=>{
-  const post = await Post.findById(req.params.id);
-  await post.remove();
-  return res.send(); 
-});
+routes.delete('/posts/:id', PostController.delete);
 
 module.exports = routes; 
