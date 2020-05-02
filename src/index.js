dc7b88e78d0e2147c955f7080bd950beaba11259
felipealvescosta <felipealves00007@gmail.com>
2020-05-02 15:47:06 -0300

    chore: new files

--- src/index.js
+++ src/index.js
@@ -3,14 +3,16 @@ const express  = require("express");
 const morgan = require("morgan");
 const mongoose = require('mongoose');
 const path = require('path'); 
+const cors = require('cors');
 
-const app = express();
+const app = express(); 
 
 mongoose.connect(process.env.DATABASE_URL,{
     useNewUrlParser: true,
     useUnifiedTopology: true
 });
- 
+
+app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({extended:true}));
 app.use(morgan("dev"));
